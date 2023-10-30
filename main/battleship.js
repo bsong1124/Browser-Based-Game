b4a31a03260c5d8e07520995b5648213dd02ba8a
bsong1124 <bsong1124@gmail.com>
2023-10-30 13:02:50 -0400

    create table and update html

--- main/battleship.js
+++ main/battleship.js
@@ -1,40 +1,44 @@
 console.log("js loaded");
+// Global Variables
+const yAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const computerTable = document.querySelector("#computer-table > table");
+const playerTable = document.querySelector("#player-table > table");
+let player;
+let computer;
+
 // Example Class
 class Player {
-  contructor(name) {
-    this.name = name; //player or computer
-
-    this.grid = {
-      // grid creation here
-      xAxis: ['a','b','c','d','e','f','g','h','i'],
-      yAxis: [1,2,3,4,5,6,7,8,9]
-    };
-
+  constructor(name, tableDOM) {
+    this.name = name;
+    this.tableDOM = tableDOM;
     this.ships = [
-      (submarine = {
-        length: 3,
-        hitPoints: 3,
-      }),
-      (cruiser = {
-        length: 3,
-        hitPoints: 3,
-      }),
-      (battleship = {
-        length: 4,
-        hitPoints: 4,
-      }),
-      (carrier = {
-        length: 5,
-        hitPoints: 5,
-      }),
+      { name: "submarine", length: 3, hitPoints: 3 },
+      { name: "cruiser", length: 3, hitPoints: 3 },
+      { name: "battleship", length: 4, hitPoints: 4 },
+      { name: "carrier", length: 5, hitPoints: 5 },
     ];
+  }
+  // Methods
+  tableCreation() {
+    for (let i = 1; i <= yAxis.length; i++) {
+      let createRow = document.createElement("tr");
+      this.tableDOM.appendChild(createRow);
+      let createRowHeader = document.createElement("th");
+      createRowHeader.textContent = i;
+      createRow.appendChild(createRowHeader);
+      for(let j=0;j<9;j++){
+        let createCell = document.createElement('td')
+        createRow.appendChild(createCell)
+      }
+    }
   }
 }
 
-init()
+init();
 
-function init(){
-  for(let i=0; i<=9;i++){
-    
-  }
+function init() {
+  computer = new Player("Computer", computerTable);
+  computer.tableCreation();
+  player = new Player("Player", playerTable);
+  player.tableCreation();
 }
