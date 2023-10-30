a086c0bd0b718b8d5a2c68233e5dd2b69d038787
bsong1124 <bsong1124@gmail.com>
2023-10-30 16:33:49 -0400

    create ship container minimal css update minimal html update`

--- main/battleship.js
+++ main/battleship.js
@@ -3,14 +3,17 @@ console.log("js loaded");
 const yAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const computerTable = document.querySelector("#computer-table > table");
 const playerTable = document.querySelector("#player-table > table");
+const computerShip = document.querySelector("#computer-ship");
+const playerShip = document.querySelector("#player-ship");
 let player;
 let computer;
 
 // Example Class
 class Player {
-  constructor(name, tableDOM) {
+  constructor(name, tableDOM, shipDOM) {
     this.name = name;
     this.tableDOM = tableDOM;
+    this.shipDOM = shipDOM;
     this.ships = [
       { name: "submarine", length: 3, hitPoints: 3 },
       { name: "cruiser", length: 3, hitPoints: 3 },
@@ -26,10 +29,20 @@ class Player {
       let createRowHeader = document.createElement("th");
       createRowHeader.textContent = i;
       createRow.appendChild(createRowHeader);
-      for(let j=0;j<9;j++){
-        let createCell = document.createElement('td')
-        createRow.appendChild(createCell)
+      for (let j = 0; j < 9; j++) {
+        let createCell = document.createElement("td");
+        createRow.appendChild(createCell);
       }
+    }
+  }
+  shipCreation() {
+    for (let ship of this.ships) {
+      let createShip = document.createElement("div");
+      createShip.setAttribute("id", ship.name);
+      createShip.classList.add(ship.name);
+      createShip.draggable = true;
+      this.shipDOM.appendChild(createShip);
+      // console.log(this.name, createShip)
     }
   }
 }
@@ -37,8 +50,13 @@ class Player {
 init();
 
 function init() {
-  computer = new Player("Computer", computerTable);
+  computer = new Player("Computer", computerTable, computerShip);
   computer.tableCreation();
-  player = new Player("Player", playerTable);
+  computer.shipCreation();
+  player = new Player("Player", playerTable, playerShip);
   player.tableCreation();
+  player.shipCreation();
+}
+function rotateShip(){
+  
 }
