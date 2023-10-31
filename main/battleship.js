70fbb93a951ce5aa47489da28973dd9a4f1f079f
bsong1124 <bsong1124@gmail.com>
2023-10-31 18:48:54 -0400

    fix random ship placement fix random ship rotation function clean up

--- main/battleship.js
+++ main/battleship.js
@@ -10,6 +10,11 @@ const resetButton = document.querySelector("#reset-button");
 const yAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let player;
 let computer;
+let computerShipArray = [];
+let computerSubmarine;
+let computerCruiser;
+let computerBattleship;
+let computerCarrier;
 // eventlisteners
 startButton.addEventListener("click", start);
 rotateButton.addEventListener("click", rotateShip);
@@ -23,10 +28,10 @@ class Player {
     this.tableDOM = tableDOM;
     this.shipDOM = shipDOM;
     this.ships = [
-      { name: "submarine", length: 3, hitPoints: 3 },
-      { name: "cruiser", length: 3, hitPoints: 3 },
-      { name: "battleship", length: 4, hitPoints: 4 },
-      { name: "carrier", length: 5, hitPoints: 5 },
+      { name: "submarine", length: 3, hitPoints: 3, coord: [] },
+      { name: "cruiser", length: 3, hitPoints: 3, coord: [] },
+      { name: "battleship", length: 4, hitPoints: 4, coord: [] },
+      { name: "carrier", length: 5, hitPoints: 5, coord: [] },
     ];
   }
   // Methods
@@ -40,7 +45,7 @@ class Player {
       for (let j = 1; j < 10; j++) {
         let createCell = document.createElement("td");
         createCell.classList.add(this.name);
-        createCell.setAttribute("id", 'x'+i+'y'+j);
+        createCell.setAttribute("id", "x" + i + "y" + j);
         createRow.appendChild(createCell);
       }
     }
@@ -48,21 +53,92 @@ class Player {
   shipCreation() {
     for (let ship of this.ships) {
       let createShip = document.createElement("div");
-      createShip.innerText = `${ship.length}x`
+      createShip.innerText = `${ship.length}x`;
       createShip.setAttribute("id", ship.name);
       createShip.classList.add(ship.name, "horizontal");
       this.shipDOM.appendChild(createShip);
-      ship.coordinates; 
+      ship.coordinates;
       if (this.shipDOM === playerShip) {
         createShip.draggable = true;
         // console.log(this.name, createShip)
       }
     }
   }
+  randomPlaceShips() {
+    this.ships.forEach(function (ship) {
+      const shipLength = ship.length;
+      while (true) {
+        // infinite loop choose two random points as a candidate for ship coords
+        const ranX = Math.floor(Math.random() * 9) + 1;
+        const ranY = Math.floor(Math.random() * 9) + 1;
+        if (ranBoolean()) {
+          if (ranY + ship.length - 1 <= 9) {
+            if (this.setShip(ship, ranY, ranX, true)) {
+              break;
+            }
+          }
+        } else if (ranX + ship.length - 1 <= 9) {
+          if (this.setShip(ship, ranY, ranX, false)) {
+            break;
+          }
+        }
+      }
+    }, this);
+  }
+  setShip(ship, ranY, ranX, isHorizontal) {
+    if (isHorizontal) {
+      for (let i = ranY; i < ranY + ship.length; i++) {
+        let cellId = "x" + ranX + "y" + i;
+        const cell = this.tableDOM.querySelector("#" + cellId);
+        if (cell.classList.contains("placed")) {
+          return false;
+        }
+      }
+      for (let i = ranY; i < ranY + ship.length; i++) {
+        let cellId = "x" + ranX + "y" + i;
+        const cell = this.tableDOM.querySelector("#" + cellId);
+        cell.classList.add("placed");
+      }
+    } else {
+      for (let i = ranX; i < ranX + ship.length; i++) {
+        let cellId = "x" + i + "y" + ranY;
+        const cell = this.tableDOM.querySelector("#" + cellId);
+        if (cell.classList.contains("placed")) {
+          return false;
+        }
+      }
+      for (let i = ranX; i < ranX + ship.length; i++) {
+        let cellId = "x" + i + "y" + ranY;
+        const cell = this.tableDOM.querySelector("#" + cellId);
+        cell.classList.add("placed");
+      }
+    }
+    return true;
+  }
 }
 
-
 init();
+
+const computerShipDivs = document.querySelectorAll("#computer-ship>div");
+const playerShipDivs = document.querySelectorAll("#player-ship>div");
+// const computerCells = document.querySelectorAll("#computer > tr > td");
+// const playerCells = document.querySelectorAll("#player > tr > td");
+
+// for (let ship of computerShipDivs) {
+//   if (ship.classList.contains("submarine")) {
+//     computerSubmarine = ship;
+//     computerShipArray.push(computerSubmarine);
+//   } else if (ship.classList.contains("cruiser")) {
+//     computerCruiser = ship;
+//     computerShipArray.push(computerCruiser);
+//   } else if (ship.classList.contains("battleship")) {
+//     computerBattleship = ship;
+//     computerShipArray.push(computerBattleship);
+//   } else if (ship.classList.contains("carrier")) {
+//     computerCarrier = ship;
+//     computerShipArray.push(computerCarrier);
+//   }
+// }
 
 function init() {
   computer = new Player("Computer", computerTable, computerShip);
@@ -71,16 +147,12 @@ function init() {
   player = new Player("Player", playerTable, playerShip);
   player.tableCreation();
   player.shipCreation();
+  computer.randomPlaceShips();
 }
 
 function start() {}
 
-const playerCells = document.querySelectorAll("#player > tr > td");
-const computerCells = document.querySelectorAll("#computer > tr > td");
-console.log(computerCells[0])
-
 function rotateShip() {
-  const playerShipDivs = document.querySelectorAll("#player-ship>div");
   playerShipDivs.forEach(function (shipDiv) {
     if (shipDiv.classList.contains("horizontal")) {
       shipDiv.style.transform = "rotate(90deg)";
@@ -92,51 +164,10 @@ function rotateShip() {
   });
 }
 
-function randomRotateShip() {
-  const computerShipDivs = document.querySelectorAll("#computer-ship>div");
-  computerShipDivs.forEach(function (shipDiv) {
-    if (ranBoolean()) {
-      shipDiv.style.transform = "rotate(90deg)";
-      shipDiv.classList.add("vertical");
-    } else {
-      shipDiv.style.transform = "rotate(0deg)";
-      shipDiv.classList.add("horizontal");
-    }
-  });
-}
-
-function randomPlaceComputerShip(ship) {
-  randomRotateShip();
-  const ranNum = Math.floor(Math.random() * 81);
-  let cellStart;
-  if (ship.classList.contains("horizontal")) {
-    if (ranNum <= 81 - ship.length) {
-      cellStart = ranNum;
-    } else {
-      cellStart = 81 - ship.length;
-    }
-    for(let i=cellStart;i<cellStart+ship.length;i++){
-      computerCells[i].classList.add(ship.name)
-      computerCells[i].classList.add('placed')
-    }
-  } else if (ship.classList.contains("vertical")) {
-    if (ranNum <= 81 - 9 * (ship.length - 1)) {
-      cellStart = ranNum;
-    } else {
-      cellStart = 81 - 9 * (ship.length - 1);
-    }
-    for(let i=cellStart;i<=cellStart+9*(ship.length-1);i+9){
-      computerCells[i].classList.add(ship.name)
-      computerCells[i].classList.add('placed')
-    }
-  }
-}
-randomPlaceComputerShip(computer.ships)
-
 function ranBoolean() {
   return Math.random() < 0.5;
 }
 
 function fireMissile() {}
 function placePlayerShips() {}
-console.log(player.ships[3].length)
+console.log(player.ships[3].length);
